4da868f0fdc5ca0eaf4b205051e230780d555b82
PaulVeevers <veevers.paul@gmail.com>
2016-02-13 23:06:28 +1100

    fix issues

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,16 @@
-'use strict';
+// var electronCookies = require('electron-cookies');
 
-var electronCookies = require('electron-cookies');
+console.log("index.js")
+console.log(Trello);
+
+Trello.authorize({
+    type: "redirect",
+    name: "Toptask",
+    scope: {
+        read: true,
+        write: false
+    },
+    expiration: "never",
+    success: function() { console.log("Successful authentication"); },
+    error: function() { console.log("Failed authentication"); }
+});
--- main.js
+++ main.js
@@ -1,33 +1,36 @@
-'use strict';
+// Dependencies
+var electron = require('electron'),
+    app = electron.app,
+    BrowserWindow = electron.BrowserWindow;
 
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+// Config
+var mainWindow;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-
+// Public methods
 function createWindow () {
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            "node-integration": false
+        }
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 }
 
 // This method will be called when Electron has finished
