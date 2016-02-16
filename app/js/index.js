d5735a472f2db1c5f525d22235caa8ea6920d46b
Shane Herft <shaneherft@gmail.com>
2016-02-16 18:41:00 +1100

    Changes to reset caches

--- app/js/index.js
+++ app/js/index.js
@@ -6,6 +6,20 @@ Trello.authorize({
         write: false
     },
     expiration: "never",
-    success: function() { console.log("Successful authentication"); },
+    success: function() { onAuthorizeSuccessful(); console.log("Successful authentication"); },
     error: function() { console.log("Failed authentication"); }
 });
+
+// var cardShort;
+//
+//
+// Trello.get('1/cards/3lSJRj2N');
+
+console.log("Check code runs up to here");
+
+function onAuthorizeSuccessful() {
+    var token = Trello.token();
+    today = new Date("December 25, 2015 12:00:00");
+    console.log(token);
+
+}
--- main.js
+++ main.js
@@ -21,6 +21,7 @@ function createWindow () {
     // and load the index.html of the app.
     mainWindow.loadURL('http://localhost:8000');
 
+
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
 
